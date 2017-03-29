b68df02493b9f0d101443400c187fc160e593cb7
arjun sanjay A-R ragunath <arjun sanjay A-R ragunath>
2017-03-29 15:20:57 +0200

    login added

--- app/php/controls/BezoekerController.php
+++ app/php/controls/BezoekerController.php
@@ -8,7 +8,26 @@ class BezoekerController extends AbstractController {
   protected $view;
   protected $model;
 
-  public function defaultAction() {
+  public function defaultAction(){
+    if($this->model->isPostLeeg()){
+        $this->view->set("note","vul uw gegevns in");
+    }
+    else{
+        $resultInlog=$this->model->controleerInloggen();
+        switch($resultInlog){
+            case REQUEST_SUCCESS:
+                $this->view->set("note","Welkom ".$_SESSION['gebruiker']->getNaam());
+                $recht = $this->model->getGebruikerRecht();
+                $this->forward("default",$recht);
+                break;
+            case REQUEST_FAILURE_DATA_INVALID:
+                $this->view->set("note","Gegevens kloppen niet, probeer het opnieuw");
+                break;
+            case REQUEST_FAILURE_DATA_INCOMPLETE:
+                $this->view->set("note","Gebruikersnaam en of wachtwoord zijn niet ingevuld");
+                break;
 
+        }
+    }
   }
 }
--- app/php/models/BezoekerModel.php
+++ app/php/models/BezoekerModel.php
@@ -2,5 +2,26 @@
 namespace php\models;
 
 class BezoekerModel extends AbstractModel {
+    private function controleerInloggen(){
+        $usn=   filter_input(INPUT_POST,'usn');
+        $ww=    filter_input(INPUT_POST,'ww');
 
+        if( ($usn!==null) && ($ww!==null) ){
+            $sql= "SELECT * FROM persons WHERE logginname = :usn AND password = :ww";
+            $sth= $this->db->prepare($sql);
+            $sth->bindParam(':usn',$usn);
+            $sth->bindParam(':ww',$ww);
+            $sth->execute();
+
+            $result = $sth->fetchAll(\PDO::FETCH_CLASS,__NAMESPACE__.'\db\Persoon');
+
+            if(count($result)===1){
+                $this->startSession();
+                $_SESSION['gebruiker']=$result[0];
+                return REQUEST_SUCCESS;
+            }
+            return REQUEST_FAILURE_DATA_INVALID;
+        }
+        return REQUEST_FAILURE_DATA_INCOMPLETE;
+    }
 }
